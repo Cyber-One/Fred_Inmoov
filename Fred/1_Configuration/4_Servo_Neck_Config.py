4d473c9f45c6c97dc0b17fad087024c8d19c69e6
Cyber-One <ray.edgley@gmail.com>
2021-04-17 19:42:49 +1000

    Update 4_Servo_Neck_Config.py
    Added more comments to the config file.

--- Fred/1_Configuration/4_Servo_Neck_Config.py
+++ Fred/1_Configuration/4_Servo_Neck_Config.py
@@ -23,7 +23,12 @@ print "Creating the Servo Neck Config"
 # Servo Neck Group                                           #
 #                                                            #
 ##############################################################
+# Depending on if you build the original version or the new
+# Roll neck version there will be 2 or 3 sesrvos installed.
 
+# The HeadYaw is used to turn the head left and right, In the
+# Official version this is mostly center back using a HS805BB
+# in Fred's modified version its a JX PDI6221MG.
 EnableHeadYaw = True
 HeadYawAttachment = "Head"      # "arduinoLeft"
 HeadYawPin = 8                  # 13
@@ -31,6 +36,11 @@ HeadYawMinPos = 0               # 30
 HeadYawMaxPos = 180             # 150
 HeadYawRestPos = 90             # 90
 
+# The HeadPitch is used to move the head up and down, this is 
+# located in the center of the torso between the front and 
+# back frames driving a piston up and down.  In the Official
+# version this is a HS805BB servo, in Fred I used a Servo 
+# Adapter and fitted a JX PDI6221MG servo.
 EnableHeadPitch = True
 HeadPitchAttachment = "Back"    # "arduinoLeft"
 HeadPitchPin = 7                # 12
@@ -38,6 +48,14 @@ HeadPitchMinPos = 0             # 20
 HeadPitchMaxPos = 180           # 160
 HeadPitchRestPos = 90           # 90
 
+# In the original version, this servo did not exist. 
+# The roll neck allows the neck plate to roll left and right
+# in turn allowing the head to maintain it's orientation when
+# the head turns left or right while looking slightly up or down
+# The roll Neck was first introduced by Bob Huston in his 
+# build and embraced by many builders, later Gael redigned
+# this to eliminate its major weaknes and has included it in
+# the Official parts list. This is still an optional update.
 EnableHeadRoll = True
 HeadRollAttachment = "Back"     # "arduinoRight"
 HeadRollPin = 6                 # 13
