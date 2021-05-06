c48a84ab7601769ef54dd6a944188b0ba5b56bb9
Cyber-One <ray.edgley@gmail.com>
2021-05-06 19:31:36 +1000

    Update 6_Servo_RightArm_Config.py

--- Fred/1_Configuration/6_Servo_RightArm_Config.py
+++ Fred/1_Configuration/6_Servo_RightArm_Config.py
@@ -13,17 +13,15 @@
 # origonal files on the Inmoov web site. http://inmoov.fr/   #
 #                                                            #
 # 6_Servo_RightArm_Config.py                                 #
-# This is where the configuration settings live for the      #
-# varoius controllers.                                       #
+# This is where the configuration settings for the Right     #
+# Arm Servos are located.                                    #
 #                                                            #
 ##############################################################
 print "Creating the Servo Right Arm Config"
-##############################################################
-#                                                            #
-# Servo Right Arm Group                                      #
-#                                                            #
-##############################################################
 
+# The Right OmoPlate lifts the arm at the shoulder out away
+# from the body, this servo is located into the torso cavity
+# and drives a rotary Piston setup in the Official InMoov Build.
 EnableRightOmoPlate = False
 RightOmoPlateAttachment = "Back"# "arduionRight"
 RightOmoPlatePin = 1            # 11
@@ -32,6 +30,8 @@ RightOmoPlateMaxPos = 180       # 80
 RightOmoPlateRestPos = 90       # 10
 RightOmoPlateMaxSpeed = 120     # 
 
+# The right shoulder is a worm drive setup the pitches the
+# right arm up in a forward direction.
 EnableRightShoulder = False
 RightShoulderAttachment="Back"  # "arduionRight"
 RightShoulderPin = 2            # 10
@@ -40,6 +40,8 @@ RightShoulderMaxPos = 180       # 180
 RightShoulderRestPos = 90       # 30
 RightShoulderMaxSpeed = 120     # 
 
+# This servo is located near the shoulder servo and rotates
+# the arm
 EnableRightRotate = False
 RightRotateAttachment = "Back"  # "arduionRight"
 RightRotatePin = 3              # 9
@@ -48,6 +50,7 @@ RightRotateMaxPin = 180         # 180
 RightRotateRestPOs = 90         # 90
 RightRotateMaxSpeed = 120         # 
 
+# This servo is located in the bicep and operates the elbow.
 EnableRightBicep = False
 RightBicepAttachment ="RightArm"# "arduioRight"
 RightBicepPin = 1               # 8
