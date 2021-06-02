7679780508858c6749de0eab53245bedc86f25af
Cyber-One <ray.edgley@gmail.com>
2021-06-03 06:34:16 +1000

    that didn't work :-)

--- Fred/OpenCV.py
+++ Fred/OpenCV.py
@@ -33,7 +33,7 @@ if EnableOpenCV:
         # check for a bounding box
         if data.getBoundingBoxArray() != None:
             for box in data.getBoundingBoxArray():
-                print("bounding box:", box.x, box.y, "|", (box.width/2)+box.x, (box.height/2)+box.y, box.width*box.height, "|", data.getBufferedImage.getWidth(), data.getBufferedImage.getHeight())
+                print("bounding box:", box.x, box.y, "|", (box.width/2)+box.x, (box.height/2)+box.y, box.width*box.height)
             print("----")
     python.subscribe("opencv", "publishOpenCVData")
     opencv.setCameraIndex(0)
