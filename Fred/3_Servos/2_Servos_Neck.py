76573a4910b923c550dd8ff85ac97ccd7f9c93fa
Cyber-One <ray.edgley@gmail.com>
2021-08-18 18:32:48 +1000

    Update 2_Servos_Neck.py

--- Fred/3_Servos/2_Servos_Neck.py
+++ Fred/3_Servos/2_Servos_Neck.py
@@ -1,44 +1,38 @@
-##############################################################
-#                                                            #
-# Program Code for Fred Inmoov                               #
-# Of the Cyber_One YouTube Channel                           #
-# https://www.youtube.com/cyber_one                          #
-#                                                            #
-# This is version 5                                          #
-# Divided up into sub programs                               #
-# Coded for the Nixie Version of MyRobotLab.                 #
-#                                                            #
-# Running on MyRobotLab (MRL) http://myrobotlab.org/         #
-# Fred in a modified Inmmov robot, you can find all the      #
-# origonal files on the Inmoov web site. http://inmoov.fr/   #
-#                                                            #
-# 2_Servos_Neck.py                                           #
-# This file is to start all the servos used in the Robot     #
-#                                                            #
-##############################################################
-print "Starting the various Servos Services"
-##############################################################
-#                                                            #
-# Servo Neck Group                                           #
-#                                                            #
-##############################################################
-# Refer to the /1_Configuration/4_Servo_Neck_Config.py for
-# more information on what each of these servos are for.
+#################################################################
+#                                                               #
+# Program Code for Fred Inmoov                                  #
+# Of the Cyber_One YouTube Channel                              #
+# https://www.youtube.com/cyber_one                             #
+#                                                               #
+# This is version 5                                             #
+# Divided up into sub programs                                  #
+# Coded for the Nixie Version of MyRobotLab.                    #
+#                                                               #
+# Running on MyRobotLab (MRL) http://myrobotlab.org/            #
+# Fred in a modified Inmmov robot, you can find all the         #
+# origonal files on the Inmoov web site. http://inmoov.fr/      #
+#                                                               #
+# 2_Servos_Neck.py                                              #
+# This file is to start all the servos used in the Robot        #
+#                                                               #
+#################################################################
 
-# Load the configuration for the Servos_Head.
+print "Starting the various Servos Services"
+#################################################################
+#                                                               #
+# Servo Neck Group                                              #
+#                                                               #
+#################################################################
+# Refer to the /1_Configuration/4_Servo_Neck_Config.py for      #
+# more information on what each of these servos are for.        #
+# Load the configuration for the Servos_Head.                   #
+#################################################################
 execfile(RuningFolder+'/1_Configuration/4_Servo_Neck_Config.py')
 
-# Test to make sure the configured controller is enabled.
-if not ((HeadYawAttachment == "Head" and EnableAdafruit16CServoDriverHead) 
-    or (HeadYawAttachment == "Back" and EnableAdafruit16CServoDriverBack) 
-    or (HeadYawAttachment == "RightArm" and EnableAdafruit16CServoDriverRightArm)
-    or (HeadYawAttachment == "LeftArm" and EnableAdafruit16CServoDriverLeftArm)
-    or (HeadYawAttachment == "arduinoNano" and EnableArduinoNano) 
-    or (HeadYawAttachment == "arduinoNano2" and EnableArduinoNano2) 
-    or (HeadYawAttachment == "arduinoLeft" and EnableArduinoLeft) 
-    or (HeadYawAttachment == "arduinoRight" and EnableArduinoRight)):
-    EnableHeadYaw = False
-
+#################################################################
+# Head Yaw allows the head to turn left and right.              #
+#################################################################
+EnableHeadYaw = TestServoControllerExists(HeadYawAttachment, EnableHeadYaw)
 if EnableHeadYaw == True:
     print "Head Yaw"
     HeadYaw = Runtime.createAndStart("HeadYaw", "Servo")
@@ -60,37 +54,14 @@ if EnableHeadYaw == True:
     HeadYaw.setAutoDisable(True)
     HeadYaw.rest()
 
-# Test to make sure the configured controller is enabled.
-if not ((HeadPitchAttachment == "Head" and EnableAdafruit16CServoDriverHead) 
-    or (HeadPitchAttachment == "Back" and EnableAdafruit16CServoDriverBack) 
-    or (HeadPitchAttachment == "RightArm" and EnableAdafruit16CServoDriverRightArm)
-    or (HeadPitchAttachment == "LeftArm" and EnableAdafruit16CServoDriverLeftArm)
-    or (HeadPitchAttachment == "arduinoNano" and EnableArduinoNano) 
-    or (HeadPitchAttachment == "arduinoNano2" and EnableArduinoNano2) 
-    or (HeadPitchAttachment == "arduinoLeft" and EnableArduinoLeft) 
-    or (HeadPitchAttachment == "arduinoRight" and EnableArduinoRight)):
-    EnableHeadPitch = False
-
+#################################################################
+# Head Pitch allows the head to turn up and down.               #
+#################################################################
+EnableHeadPitch = TestServoControllerExists(HeadPitchAttachment, EnableHeadPitch)
 if EnableHeadPitch == True:
     print "--Head Pitch"
     HeadPitch = Runtime.createAndStart("HeadPitch", "Servo")
-    if HeadPitchAttachment == "Head":
-        HeadPitch.attach(Head, HeadPitchPin)
-    elif HeadPitchAttachment == "Back":
-        HeadPitch.attach(Back, HeadPitchPin)
-    elif HeadPitchAttachment == "RightArm":
-        HeadPitch.attach(RightArm, HeadPitchPin)
-    elif HeadPitchAttachment == "LeftArm":
-        HeadPitch.attach(LeftArm, HeadPitchPin)
-    elif HeadPitchAttachment == "arduinoLeft":
-        HeadPitch.attach(arduinoLeft, HeadPitchPin)
-    elif HeadPitchAttachment == "arduinoRight":
-        HeadPitch.attach(arduinoRight, HeadPitchPin)
-    elif HeadPitchAttachment == "arduinoNano":
-        HeadPitch.attach(arduinoNano, HeadPitchPin)
-    elif HeadPitchAttachment == "arduinoNano2":
-        HeadPitch.attach(arduinoNano2, HeadPitchPin)
-    #HeadPitch.setMinMax(HeadPitchMinPos, HeadPitchMaxPos)
+    HeadPitch.attach(runtime.getService(HeadPitchAttachment), HeadPitchPin)
     HeadPitch.setMinMax(0, 100)
     if HeadPitchMinPos < HeadPitchMaxPos:
         HeadPitch.map(0, 100, HeadPitchMinPos, HeadPitchMaxPos)
@@ -106,37 +77,14 @@ if EnableHeadPitch == True:
     HeadPitch.setAutoDisable(True)
     HeadPitch.rest()
 
-# Test to make sure the configured controller is enabled.
-if not ((HeadRollAttachment == "Head" and EnableAdafruit16CServoDriverHead) 
-    or (HeadRollAttachment == "Back" and EnableAdafruit16CServoDriverBack) 
-    or (HeadRollAttachment == "RightArm" and EnableAdafruit16CServoDriverRightArm)
-    or (HeadRollAttachment == "LeftArm" and EnableAdafruit16CServoDriverLeftArm)
-    or (HeadRollAttachment == "arduinoNano" and EnableArduinoNano) 
-    or (HeadRollAttachment == "arduinoNano2" and EnableArduinoNano2) 
-    or (HeadRollAttachment == "arduinoLeft" and EnableArduinoLeft) 
-    or (HeadRollAttachment == "arduinoRight" and EnableArduinoRight)):
-    EnableHeadRoll = False
-
+#################################################################
+# Head Roll allows the head to roll from left to right.         #
+#################################################################
+EnableHeadRoll = TestServoControllerExists(HeadRollAttachment, EnableHeadRoll)
 if EnableHeadRoll == True:
     print "--Head Roll"
     HeadRoll = Runtime.createAndStart("HeadRoll", "Servo")
-    if HeadRollAttachment == "Head":
-        HeadRoll.attach(Head, HeadRollPin)
-    elif HeadRollAttachment == "Back":
-        HeadRoll.attach(Back, HeadRollPin)
-    elif HeadRollAttachment == "RightArm":
-        HeadRoll.attach(RightArm, HeadRollPin)
-    elif HeadRollAttachment == "LeftArm":
-        HeadRoll.attach(LeftArm, HeadRollPin)
-    elif HeadRollAttachment == "arduinoLeft":
-        HeadRoll.attach(arduinoLeft, HeadRollPin)
-    elif HeadRollAttachment == "arduinoRight":
-        HeadRoll.attach(arduinoRight, HeadRollPin)
-    elif HeadRollAttachment == "arduinoNano":
-        HeadRoll.attach(arduinoNano, HeadRollPin)
-    elif HeadRollAttachment == "arduinoNano2":
-        HeadRoll.attach(arduinoNano2, HeadRollPin)
-    #HeadRoll.setMinMax(HeadRollMinPos, HeadRollMaxPos)
+    HeadRoll.attach(runtime.getService(HeadRollAttachment), HeadRollPin)
     HeadRoll.setMinMax(0, 100)
     if HeadRollMinPos < HeadRollMaxPos:
         HeadRoll.map(0, 100, HeadRollMinPos, HeadRollMaxPos)
