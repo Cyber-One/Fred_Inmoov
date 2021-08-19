69a774646259f3d32c3f1690a00892c879987ce2
Cyber-One <ray.edgley@gmail.com>
2021-08-19 17:47:30 +1000

    Update 3_Servos_Torso.py

--- Fred/3_Servos/3_Servos_Torso.py
+++ Fred/3_Servos/3_Servos_Torso.py
@@ -29,36 +29,11 @@ print "Starting the various Servos Services"
 execfile(RuningFolder+'/1_Configuration/5_Servo_Torso_Config.py')
 
 # Test to make sure the configured controller is enabled.
-if not ((TopStomachAttachment == "Head" and EnableAdafruit16CServoDriverHead) 
-    or (TopStomachAttachment == "Back" and EnableAdafruit16CServoDriverBack) 
-    or (TopStomachAttachment == "RightArm" and EnableAdafruit16CServoDriverRightArm)
-    or (TopStomachAttachment == "LeftArm" and EnableAdafruit16CServoDriverLeftArm)
-    or (TopStomachAttachment == "arduinoNano" and EnableArduinoNano) 
-    or (TopStomachAttachment == "arduinoNano2" and EnableArduinoNano2) 
-    or (TopStomachAttachment == "arduinoLeft" and EnableArduinoLeft) 
-    or (TopStomachAttachment == "arduinoRight" and EnableArduinoRight)):
-    EnableTopStomach = False
-
+EnableTopStomach = TestServoControllerExists(TopStomachAttachment, EnableTopStomach)
 if EnableTopStomach == True:
     print "--Top Stomach"
     TopStomach = Runtime.createAndStart("TopStomach", "Servo")
-    if TopStomachAttachment == "Head":
-        TopStomach.attach(Head, TopStomachPin)
-    elif TopStomachAttachment == "Back":
-        TopStomach.attach(Back, TopStomachPin)
-    elif TopStomachAttachment == "RightArm":
-        TopStomach.attach(RightArm, TopStomachPin)
-    elif TopStomachAttachment == "LeftArm":
-        TopStomach.attach(LeftArm, TopStomachPin)
-    elif TopStomachAttachment == "arduinoLeft":
-        TopStomach.attach(arduinoLeft, TopStomachPin)
-    elif TopStomachAttachment == "arduinoRight":
-        TopStomach.attach(arduinoRight, TopStomachPin)
-    elif TopStomachAttachment == "arduinoNano":
-        TopStomach.attach(arduinoNano, TopStomachPin)
-    elif TopStomachAttachment == "arduinoNano2":
-        TopStomach.attach(arduinoNano2, TopStomachPin)
-    #TopStomach.setMinMax(TopStomachMinPos, TopStomachMaxPos)
+    TopStomach.attach(runtime.getService(TopStomachAttachment), TopStomachPin)
     TopStomach.setMinMax(0, 100)
     if TopStomachMinPos < TopStomachMaxPos:
         TopStomach.map(0, 100, TopStomachMinPos, TopStomachMaxPos)
@@ -75,36 +50,11 @@ if EnableTopStomach == True:
     TopStomach.rest()
 
 # Test to make sure the configured controller is enabled.
-if not ((MidStomachAttachment == "Head" and EnableAdafruit16CServoDriverHead) 
-    or (MidStomachAttachment == "Back" and EnableAdafruit16CServoDriverBack) 
-    or (MidStomachAttachment == "RightArm" and EnableAdafruit16CServoDriverRightArm)
-    or (MidStomachAttachment == "LeftArm" and EnableAdafruit16CServoDriverLeftArm)
-    or (MidStomachAttachment == "arduinoNano" and EnableArduinoNano) 
-    or (MidStomachAttachment == "arduinoNano2" and EnableArduinoNano2) 
-    or (MidStomachAttachment == "arduinoLeft" and EnableArduinoLeft) 
-    or (MidStomachAttachment == "arduinoRight" and EnableArduinoRight)):
-    EnableMidStomach = False
-
+EnableMidStomach = TestServoControllerExists(MidStomachAttachment, EnableMidStomach)
 if EnableMidStomach == True:
     print "--Top Stomach"
     MidStomach = Runtime.createAndStart("MidStomach", "Servo")
-    if MidStomachAttachment == "Head":
-        MidStomach.attach(Head, MidStomachPin)
-    elif MidStomachAttachment == "Back":
-        MidStomach.attach(Back, MidStomachPin)
-    elif MidStomachAttachment == "RightArm":
-        MidStomach.attach(RightArm, MidStomachPin)
-    elif MidStomachAttachment == "LeftArm":
-        MidStomach.attach(LeftArm, MidStomachPin)
-    elif MidStomachAttachment == "arduinoLeft":
-        MidStomach.attach(arduinoLeft, MidStomachPin)
-    elif MidStomachAttachment == "arduinoRight":
-        MidStomach.attach(arduinoRight, MidStomachPin)
-    elif MidStomachAttachment == "arduinoNano":
-        MidStomach.attach(arduinoNano, MidStomachPin)
-    elif MidStomachAttachment == "arduinoNano2":
-        MidStomach.attach(arduinoNano2, MidStomachPin)
-    #MidStomach.setMinMax(MidStomachMinPos, MidStomachMaxPos)
+    MidStomach.attach(runtime.getService(MidStomachAttachment), MidStomachPin)
     MidStomach.setMinMax(0, 100)
     if MidStomachMinPos < MidStomachMaxPos:
         MidStomach.map(0, 100, MidStomachMinPos, MidStomachMaxPos)
@@ -121,36 +71,12 @@ if EnableMidStomach == True:
     MidStomach.rest()
 
 # Test to make sure the configured controller is enabled.
-if not ((PitchStomachAttchment == "Head" and EnableAdafruit16CServoDriverHead) 
-    or (PitchStomachAttchment == "Back" and EnableAdafruit16CServoDriverBack) 
-    or (PitchStomachAttchment == "RightArm" and EnableAdafruit16CServoDriverRightArm)
-    or (PitchStomachAttchment == "LeftArm" and EnableAdafruit16CServoDriverLeftArm)
-    or (PitchStomachAttchment == "arduinoNano" and EnableArduinoNano) 
-    or (PitchStomachAttchment == "arduinoNano2" and EnableArduinoNano2) 
-    or (PitchStomachAttchment == "arduinoLeft" and EnableArduinoLeft) 
-    or (PitchStomachAttchment == "arduinoRight" and EnableArduinoRight)):
-    EnablePitchStomach = False
+EnablePitchStomach = TestServoControllerExists(PitchStomachAttchment, EnablePitchStomach)
 
 if EnablePitchStomach == True:
     print "--Top Stomach"
     PitchStomach = Runtime.createAndStart("PitchStomach", "Servo")
-    if PitchStomachAttchment == "Head":
-        PitchStomach.attach(Head, PitchStomachPin)
-    elif PitchStomachAttchment == "Back":
-        PitchStomach.attach(Back, PitchStomachPin)
-    elif PitchStomachAttchment == "RightArm":
-        PitchStomach.attach(RightArm, PitchStomachPin)
-    elif PitchStomachAttchment == "LeftArm":
-        PitchStomach.attach(LeftArm, PitchStomachPin)
-    elif PitchStomachAttchment == "arduinoLeft":
-        PitchStomach.attach(arduinoLeft, PitchStomachPin)
-    elif PitchStomachAttchment == "arduinoRight":
-        PitchStomach.attach(arduinoRight, PitchStomachPin)
-    elif PitchStomachAttchment == "arduinoNano":
-        PitchStomach.attach(arduinoNano, PitchStomachPin)
-    elif PitchStomachAttchment == "arduinoNano2":
-        PitchStomach.attach(arduinoNano2, PitchStomachPin)
-    #PitchStomach.setMinMax(PitchStomachMinPos, PitchStomachMaxPos)
+    PitchStomach.attach(runtime.getService(PitchStomachAttchment), PitchStomachPin)
     PitchStomach.setMinMax(0, 100)
     if PitchStomachMinPos < PitchStomachMaxPos:
         PitchStomach.map(0, 100, PitchStomachMinPos, PitchStomachMaxPos)
